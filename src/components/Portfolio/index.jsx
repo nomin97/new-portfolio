fad66b90c9755af847e0f0d3bc88e774624b9c48
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2024-08-04 22:58:14 -0600

    updated descriptions of projects

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -7,19 +7,19 @@ function Portfolio() {
   const [projects] = useState([
     {
       name: 'poppinMovies',
-      description: 'Interactive Front End',
+      description: 'An interactive front-end app enabling users to search for movies and TV shows, and discover streaming, renting, or purchasing options.',
       link: "https://akleynhans.github.io/Popping-Movies/",
       repo: "https://github.com/Akleynhans/Popping-Movies"
     },
     {
       name: 'twitwit',
-      description: 'Full Stack',
+      description: 'A comprehensive full-stack social media platform enabling user registration, authentication, posting of thoughts, and interaction with other users thoughts.',
       link: "https://twitwit-3030f5d6caaa.herokuapp.com/",
       repo: "https://github.com/Blakewb3/Twitwit"
     },
     {
       name: 'pawPrint',
-      description: 'MERN Stack',
+      description: 'An interactive MERN Stack employee side kennel application that allows the employee to create their account, add/update dog and owner profiles, and update rooms.',
       link: "https://paw-print.onrender.com/",
       repo: "https://github.com/pcstoyle/paw-print"
     },
