ea837a46b8bc462fa948bde423f5d1e59e3ca2ea
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2024-08-04 18:21:57 -0600

    fixed image not loading issue

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -6,19 +6,19 @@ function Portfolio() {
   // Replace links with deployed projects and GitHub repos
   const [projects] = useState([
     {
-      name: 'Poppin Movies',
+      name: 'poppinMovies',
       description: 'Interactive Front End',
       link: "https://akleynhans.github.io/Popping-Movies/",
       repo: "https://github.com/Akleynhans/Popping-Movies"
     },
     {
-      name: 'TwitWit',
+      name: 'twitwit',
       description: 'Full Stack',
       link: "https://twitwit-3030f5d6caaa.herokuapp.com/",
       repo: "https://github.com/Blakewb3/Twitwit"
     },
     {
-      name: 'Paw Print',
+      name: 'pawPrint',
       description: 'MERN Stack',
       link: "https://paw-print.onrender.com/",
       repo: "https://github.com/pcstoyle/paw-print"
